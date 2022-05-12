58f1a7ef03f40ad24a2f17dc4a3b292272aadf62
Ahsan Mukhtar <ahsan44411@gmail.com>
2022-05-12 23:45:57 +0500

    completed

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import slider6 from "./assets/images/slider 6.png";
 import slider7 from "./assets/images/slider 7.png";
 import slider8 from "./assets/images/slider 8.png";
 import slider9 from "./assets/images/slider 9.png";
+import loaderimg from './assets/images/loader.png'
+import welcome_text from './assets/images/welcome-text.png';
 import MintDetails from "./Components/MintDetails";
 import Reveal from "./Components/Reveal";
 import RoadMap from "./Components/RoadMap";
@@ -21,7 +23,7 @@ import JoinUs from "./Components/JoinUs";
 import Faq from "./Components/FAQ";
 import Footer from "./Components/Footer";
 import Header from './Components/header';
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 function App() {
     const heroRef = useRef(null)
@@ -40,66 +42,110 @@ function App() {
         ref.current.scrollIntoView({behavior: 'smooth'})
     }
 
+    const logKey = (e) => {
+        if (e.code === 'KeyR') {
+            setRpressed(true)
+        }
+    }
+
+    document.addEventListener('keydown', logKey);
+
+    const [rPressed, setRpressed] = useState(false)
+    const [loader, setLoader] = useState(false)
+
+    useEffect(() => {
+        if (rPressed) {
+            setTimeout(
+                () => {
+                    setLoader(true)
+                }, 2000
+            );
+        }
+    }, [rPressed])
+
+    const enter_clicked = () => {
+        setLoader(false)
+        setRpressed(false)
+    }
+
     return (
         <>
-            <Header
-                openHeader={openHeader}
-                setOpenHeader={setOpenHeader}
-                scroll={scroll}
-                heroRef={heroRef}
-                whyRef={whyRef}
-                meetRef={meetRef}
-                detailRef={detailRef}
-                revealRef={revealRef}
-                mapRef={mapRef}
-                joinUsRef={joinUsRef}
-                faqRef={faqRef}
-            />
+            {rPressed ?
+                !loader ?
+                    <div className={'loader-container'}>
+                        <div style={{margin: 'auto'}}>
+                            <img src={loaderimg} style={{width: '100%'}}/>
+                        </div>
+                    </div>
+                    :
+                    <div className={'welcome-page'}>
+                        <div style={{margin: 'auto', textAlign: 'center'}}>
+                            <img src={welcome_text} style={{width: '100%'}}/>
+                            <button className={'btn enter-btn'} onClick={enter_clicked}>Enter</button>
+                        </div>
+                    </div>
+                :
+                <>
+                    <Header
+                        openHeader={openHeader}
+                        setOpenHeader={setOpenHeader}
+                        scroll={scroll}
+                        heroRef={heroRef}
+                        whyRef={whyRef}
+                        meetRef={meetRef}
+                        detailRef={detailRef}
+                        revealRef={revealRef}
+                        mapRef={mapRef}
+                        joinUsRef={joinUsRef}
+                        faqRef={faqRef}
+                    />
 
-            <div ref={heroRef}>
-                <Hero/>
-            </div>
-            <div style={{width: '100%'}}>
-                <img src={tiger} style={{width: '100%'}}/>
-            </div>
-            <div ref={whyRef}>
-                <WhyZzoppers/>
-            </div>
+                    <div ref={heroRef}>
+                        <Hero/>
+                    </div>
+                    <div style={{width: '100%'}}>
+                        <img src={tiger} style={{width: '100%'}}/>
+                    </div>
+                    <div ref={whyRef}>
+                        <WhyZzoppers/>
+                    </div>
 
-            <Utils/>
+                    <Utils/>
 
-            <div ref={meetRef}>
-                <Meet/>
-            </div>
-            <Scrollers
-                scroller_images={[slider1, slider2, slider3, slider4, slider5, slider6, slider7, slider8, slider9]}/>
-            <Scrollers
-                scroller_images={[slider9, slider4, slider5, slider6, slider7, slider8, slider1, slider2, slider3]}/>
-            <Scrollers
-                scroller_images={[slider2, slider1, slider5, slider6, slider3, slider9, slider4, slider7, slider8]}/>
+                    <div ref={meetRef}>
+                        <Meet/>
+                    </div>
+                    <Scrollers
+                        scroller_images={[slider1, slider2, slider3, slider4, slider5, slider6, slider7, slider8, slider9]}/>
+                    <Scrollers
+                        scroller_images={[slider9, slider4, slider5, slider6, slider7, slider8, slider1, slider2, slider3]}/>
+                    <Scrollers
+                        scroller_images={[slider2, slider1, slider5, slider6, slider3, slider9, slider4, slider7, slider8]}/>
 
-            <div ref={detailRef}>
-                <MintDetails/>
-            </div>
-            <div ref={revealRef}>
-                <Reveal/>
-            </div>
+                    <div ref={detailRef}>
+                        <MintDetails/>
+                    </div>
+                    <div ref={revealRef}>
+                        <Reveal/>
+                    </div>
 
-            <div ref={mapRef}>
-                <RoadMap/>
-            </div>
+                    <div ref={mapRef}>
+                        <RoadMap/>
+                    </div>
 
-            <Create/>
+                    <Create/>
 
-            <div ref={joinUsRef}>
-                <JoinUs/>
-            </div>
+                    <div ref={joinUsRef}>
+                        <JoinUs/>
+                    </div>
 
-            <div ref={faqRef}>
-                <Faq/>
-            </div>
+                    <div ref={faqRef}>
+                        <Faq/>
+                    </div>
 
-            <Footer/>
+                    <Footer/>
+                </>
+            }
         </>
     );
 }
